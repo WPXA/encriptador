502a8a4b4b90372f6703fff7063c7e348d33e16c
WPXA <wolfpaxi@hotmail.com>
2023-05-14 14:34:29 -0500

    de zero

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function encriptar() {
-    var texto = document.getElementById("texto").value;
+    var texto = document.getElementById("area-texto-uno").value;
     var resultado = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
-    document.getElementById("resultado").value = resultado;
+    document.getElementById("area-texto-dos").value = resultado;
   }
   
   function desencriptar() {
-    var texto = document.getElementById("texto").value;
+    var texto = document.getElementById("area-texto-uno").value;
     var resultado = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-    document.getElementById("resultado").value = resultado;
+    document.getElementById("area-texto-dos").value = resultado;
   }
   
   
